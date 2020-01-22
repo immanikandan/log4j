f0e3571c6cb241d636102ed4af530a0429a29dc0
Ganesh RP <ganesh.r@10decoders.in>
2020-01-22 12:26:05 +0530

    My Learning

---
+++ log4j/src/com/log4j/TestLog.java
@@ -0,0 +1,25 @@
+package com.log4j;
+
+import org.apache.log4j.Logger;
+
+public class TestLog {
+	void devide() {
+		int a=10/0;
+	}
+	final static Logger l=Logger.getLogger(TestLog.class);
+	public static void main(String[] args) {
+		l.debug("DEBUG MSG");
+		l.info("INFO MSG");
+		l.warn("WARN MSG");
+		l.error("ERROR MSG");
+		l.fatal("FATAL MSG");
+		try {
+			TestLog t=new TestLog();
+			t.devide();
+		}
+		catch(Exception e) {
+			l.error("Arithmetic Exception Found");
+		}
+	}
+
+}
